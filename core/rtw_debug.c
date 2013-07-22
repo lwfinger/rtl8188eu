cc7f562a555854aa966f0dce9a8132fffc52118c
Larry Finger <Larry.Finger@lwfinger.net>
2013-07-21 21:36:22 -0500

    rtl8188eu: Remove CONFIG_DEBUG_RTL871X - not defined
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_debug.c
+++ core/rtw_debug.c
@@ -22,43 +22,6 @@
 #include <rtw_debug.h>
 #include <rtw_version.h>
 
-#ifdef CONFIG_DEBUG_RTL871X
-
-	u32 GlobalDebugLevel = _drv_err_;
-
-	u64 GlobalDebugComponents = \
-			_module_rtl871x_xmit_c_ |
-			_module_xmit_osdep_c_ |
-			_module_rtl871x_recv_c_ |
-			_module_recv_osdep_c_ |
-			_module_rtl871x_mlme_c_ |
-			_module_mlme_osdep_c_ |
-			_module_rtl871x_sta_mgt_c_ |
-			_module_rtl871x_cmd_c_ |
-			_module_cmd_osdep_c_ |
-			_module_rtl871x_io_c_ |
-			_module_io_osdep_c_ |
-			_module_os_intfs_c_|
-			_module_rtl871x_security_c_|
-			_module_rtl871x_eeprom_c_|
-			_module_hal_init_c_|
-			_module_hci_hal_init_c_|
-			_module_rtl871x_ioctl_c_|
-			_module_rtl871x_ioctl_set_c_|
-			_module_rtl871x_ioctl_query_c_|
-			_module_rtl871x_pwrctrl_c_|
-			_module_hci_intfs_c_|
-			_module_hci_ops_c_|
-			_module_hci_ops_os_c_|
-			_module_rtl871x_ioctl_os_c|
-			_module_rtl8712_cmd_c_|
-			_module_hal_xmit_c_|
-			_module_rtl8712_recv_c_ |
-			_module_mp_ |
-			_module_efuse_;
-
-#endif
-
 int proc_get_drv_version(char *page, char **start,
 			  off_t offset, int count,
 			  int *eof, void *data)
