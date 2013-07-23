3c0fdf5c36d3f9992f8b4232772dfc0d1a68e142
Larry Finger <Larry.Finger@lwfinger.net>
2013-07-23 11:12:23 -0500

    rtl8188eu: Remove typedef statements from include/rtl8188e_recv.h
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/rtl8188e_recv.h
+++ include/rtl8188e_recv.h
@@ -48,98 +48,12 @@ struct phy_stat {
 // Rx smooth factor
 #define	Rx_Smooth_Factor (20)
 
-
-typedef enum _RX_PACKET_TYPE{
+enum rx_packet_type {
 	NORMAL_RX,//Normal rx packet
 	TX_REPORT1,//CCX
 	TX_REPORT2,//TX RPT
 	HIS_REPORT,// USB HISR RPT
-}RX_PACKET_TYPE, *PRX_PACKET_TYPE;
-
-typedef struct rxreport_8188e
-{
-	//Offset 0
-	u32 pktlen:14;
-	u32 crc32:1;
-	u32 icverr:1;
-	u32 drvinfosize:4;
-	u32 security:3;
-	u32 qos:1;
-	u32 shift:2;
-	u32 physt:1;
-	u32 swdec:1;
-	u32 ls:1;
-	u32 fs:1;
-	u32 eor:1;
-	u32 own:1;
-
-	//Offset 4
-	u32 macid:5;
-	u32 tid:4;
-	u32 hwrsvd:4;
-	u32 amsdu:1;
-	u32 paggr:1;
-	u32 faggr:1;
-	u32 a1fit:4;
-	u32 a2fit:4;
-	u32 pam:1;
-	u32 pwr:1;
-	u32 md:1;
-	u32 mf:1;
-	u32 type:2;
-	u32 mc:1;
-	u32 bc:1;
-
-	//Offset 8
-	u32 seq:12;
-	u32 frag:4;
-	u32 nextpktlen:14;
-	u32 nextind:1;
-	u32 rsvd0831:1;
-
-	//Offset 12
-	u32 rxmcs:6;
-	u32 rxht:1;
-	u32 gf:1;
-	u32 splcp:1;
-	u32 bw:1;
-	u32 htc:1;
-	u32 eosp:1;
-	u32 bssidfit:2;
-	u32 rpt_sel:2;
-	u32 rsvd1216:13;
-	u32 pattern_match:1;
-	u32 unicastwake:1;
-	u32 magicwake:1;
-
-	//Offset 16
-	/*
-	u32 pattern0match:1;
-	u32 pattern1match:1;
-	u32 pattern2match:1;
-	u32 pattern3match:1;
-	u32 pattern4match:1;
-	u32 pattern5match:1;
-	u32 pattern6match:1;
-	u32 pattern7match:1;
-	u32 pattern8match:1;
-	u32 pattern9match:1;
-	u32 patternamatch:1;
-	u32 patternbmatch:1;
-	u32 patterncmatch:1;
-	u32 rsvd1613:19;
-	*/
-	u32 rsvd16;
-
-	//Offset 20
-	u32 tsfl;
-
-	//Offset 24
-	u32 bassn:12;
-	u32 bavld:1;
-	u32 rsvd2413:19;
-} RXREPORT, *PRXREPORT;
-
+};
 
 #define INTERRUPT_MSG_FORMAT_LEN 60
 void rtl8188eu_init_recvbuf(_adapter *padapter, struct recv_buf *precvbuf);
