d1802e72f41617ec60e42a9db2d3490c125b2ff1
Larry Finger <Larry.Finger@lwfinger.net>
2013-07-27 00:08:03 -0500

    rtl8188eu: Remove RTW_ENABLE_WIFI_CONTROL_FUNC - not defined
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/rtw_android.h
+++ include/rtw_android.h
@@ -61,18 +61,7 @@ enum ANDROID_WIFI_CMD {
 int rtw_android_cmdstr_to_num(char *cmdstr);
 int rtw_android_priv_cmd(struct net_device *net, struct ifreq *ifr, int cmd);
 
-#if defined(RTW_ENABLE_WIFI_CONTROL_FUNC)
-int rtw_android_wifictrl_func_add(void);
-void rtw_android_wifictrl_func_del(void);
-void* wl_android_prealloc(int section, unsigned long size);
-
-int wifi_get_irq_number(unsigned long *irq_flags_ptr);
-int wifi_set_power(int on, unsigned long msec);
-int wifi_get_mac_addr(unsigned char *buf);
-void *wifi_get_country_code(char *ccode);
-#else
 static int rtw_android_wifictrl_func_add(void) { return 0; }
 static void rtw_android_wifictrl_func_del(void) {}
-#endif /* defined(RTW_ENABLE_WIFI_CONTROL_FUNC) */
 
 #endif //__RTW_ANDROID_H__
