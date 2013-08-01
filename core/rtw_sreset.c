79b808ad1396e5606a82a91c637c4b4a4a47efb3
Larry Finger <Larry.Finger@lwfinger.net>
2013-08-01 13:10:46 -0500

    rtl8188eu: Fix checkpatch errors in core/rtw_sreset.c
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_sreset.c
+++ core/rtw_sreset.c
@@ -28,8 +28,8 @@ void sreset_init_value(struct adapter *padapter)
 	_rtw_mutex_init(&psrtpriv->silentreset_mutex);
 	psrtpriv->silent_reset_inprogress = false;
 	psrtpriv->Wifi_Error_Status = WIFI_STATUS_SUCCESS;
-	psrtpriv->last_tx_time =0;
-	psrtpriv->last_tx_complete_time =0;
+	psrtpriv->last_tx_time = 0;
+	psrtpriv->last_tx_complete_time = 0;
 }
 void sreset_reset_value(struct adapter *padapter)
 {
@@ -38,8 +38,8 @@ void sreset_reset_value(struct adapter *padapter)
 
 	psrtpriv->silent_reset_inprogress = false;
 	psrtpriv->Wifi_Error_Status = WIFI_STATUS_SUCCESS;
-	psrtpriv->last_tx_time =0;
-	psrtpriv->last_tx_complete_time =0;
+	psrtpriv->last_tx_time = 0;
+	psrtpriv->last_tx_complete_time = 0;
 }
 
 u8 sreset_get_wifi_status(struct adapter *padapter)
@@ -50,25 +50,21 @@ u8 sreset_get_wifi_status(struct adapter *padapter)
 	u8 status = WIFI_STATUS_SUCCESS;
 	u32 val32 = 0;
 	unsigned long irqL;
-	if (psrtpriv->silent_reset_inprogress == true)
-        {
+	if (psrtpriv->silent_reset_inprogress)
 		return status;
-	}
-	val32 =rtw_read32(padapter,REG_TXDMA_STATUS);
-	if (val32==0xeaeaeaea){
+	val32 = rtw_read32(padapter, REG_TXDMA_STATUS);
+	if (val32 == 0xeaeaeaea) {
 		psrtpriv->Wifi_Error_Status = WIFI_IF_NOT_EXIST;
-	}
-	else if (val32!=0){
-		DBG_88E("txdmastatu(%x)\n",val32);
+	} else if (val32 != 0) {
+		DBG_88E("txdmastatu(%x)\n", val32);
 		psrtpriv->Wifi_Error_Status = WIFI_MAC_TXDMA_ERROR;
 	}
 
-	if (WIFI_STATUS_SUCCESS !=psrtpriv->Wifi_Error_Status)
-	{
-		DBG_88E("==>%s error_status(0x%x)\n",__func__,psrtpriv->Wifi_Error_Status);
-		status = (psrtpriv->Wifi_Error_Status &( ~(USB_READ_PORT_FAIL|USB_WRITE_PORT_FAIL)));
+	if (WIFI_STATUS_SUCCESS != psrtpriv->Wifi_Error_Status) {
+		DBG_88E("==>%s error_status(0x%x)\n", __func__, psrtpriv->Wifi_Error_Status);
+		status = (psrtpriv->Wifi_Error_Status & (~(USB_READ_PORT_FAIL|USB_WRITE_PORT_FAIL)));
 	}
-	DBG_88E("==> %s wifi_status(0x%x)\n",__func__,status);
+	DBG_88E("==> %s wifi_status(0x%x)\n", __func__, status);
 
 	/* status restore */
 	psrtpriv->Wifi_Error_Status = WIFI_STATUS_SUCCESS;
