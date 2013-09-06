aa4ccbe53f432794d409993fabdb6f6d22aa0a6e
Larry Finger <Larry.Finger@lwfinger.net>
2013-09-06 10:04:31 -0500

    rtl8188eu: Remove unused static data defines from core/rtw_recv.c
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_recv.c
+++ core/rtw_recv.c
@@ -32,13 +32,6 @@
 static u8 SNAP_ETH_TYPE_IPX[2] = {0x81, 0x37};
 
 static u8 SNAP_ETH_TYPE_APPLETALK_AARP[2] = {0x80, 0xf3};
-static u8 SNAP_ETH_TYPE_APPLETALK_DDP[2] = {0x80, 0x9b};
-static u8 SNAP_ETH_TYPE_TDLS[2] = {0x89, 0x0d};
-/*  Datagram Delivery Protocol */
-static u8 SNAP_HDR_APPLETALK_DDP[3] = {0x08, 0x00, 0x07};
-
-static u8 oui_8021h[] = {0x00, 0x00, 0xf8};
-static u8 oui_rfc1042[] = {0x00, 0x00, 0x00};
 
 static u8 rtw_rfc1042_header[] = {
 	0xaa, 0xaa, 0x03, 0x00, 0x00, 0x00
