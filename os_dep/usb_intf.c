bf9abe2b9ee33fc1933eddd3c24355839a19a41f
Hiroki Kondo <kompiro@gmail.com>
2014-08-24 14:59:41 +0900

    Add a new ID

--- os_dep/usb_intf.c
+++ os_dep/usb_intf.c
@@ -58,6 +58,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(0x2001, 0x330F)}, /* DLink DWA-125 REV D1 */
         {USB_DEVICE(0x2001, 0x3310)}, /* Dlink DWA-123 REV D1 */
 	{USB_DEVICE(0x2001, 0x3311)}, /* DLink GO-USB-N150 REV B1 */
+	{USB_DEVICE(0x056E, 0x4008)}, /* Elecom WDC-150SU2M */
 	{}	/* Terminating entry */
 };
 
