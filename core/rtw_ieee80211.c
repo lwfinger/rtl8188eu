26930b0f9ff7f2988dd91ec678029771d6ed0e20
Larry Finger <Larry.Finger@lwfinger.net>
2014-12-12 22:30:01 -0600

    rtl8188eu: Replace RTW_PUT_LE16
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_ieee80211.c
+++ core/rtw_ieee80211.c
@@ -206,8 +206,8 @@ inline u8 *rtw_set_ie_mesh_ch_switch_parm(u8 *buf, u32 *buf_len, u8 ttl,
 
 	ie_data[0] = ttl;
 	ie_data[1] = flags;
-	RTW_PUT_LE16((u8 *)&ie_data[2], reason);
-	RTW_PUT_LE16((u8 *)&ie_data[4], precedence);
+	*(u16 *)(ie_data+2) = cpu_to_le16(reason);
+	*(u16 *)(ie_data+4) = cpu_to_le16(precedence);
 
 	return rtw_set_ie(buf, 0x118,  6, ie_data, buf_len);
 }
