ff0540d9ca9b84c11b6ff1a3e740afb58dd7f830
Larry Finger <Larry.Finger@lwfinger.net>
2015-04-07 10:30:50 -0500

    rtl8188eu: Fix length of rtw_ieee80211_ht_cap
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/wifi.h
+++ include/wifi.h
@@ -632,7 +632,10 @@ struct rtw_ieee80211_bar {
 struct rtw_ieee80211_ht_cap {
 	__le16	cap_info;
 	u8	ampdu_params_info;
-	u8	supp_mcs_set[16];
+	union {
+		struct ieee80211_mcs_info mcs;
+		u8 supp_mcs_set[16];
+	};
 	__le16	extended_ht_cap_info;
 	__le32	tx_BF_cap_info;
 	u8   antenna_selection_info;
