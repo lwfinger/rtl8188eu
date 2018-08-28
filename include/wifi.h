5a6d03456529bd28cf42d4f864c8487b760fa522
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-08-28 02:15:42 +0100

    IEEE80211_MAX_AMPDU_BUF is bumped in 4.19-rc1 - avoid redefine warning
    Ref: https://github.com/torvalds/linux/commit/b8042b3da925f390c1482bf9dc0898dc0b3ea7b5

--- include/wifi.h
+++ include/wifi.h
@@ -712,7 +712,9 @@ enum HT_CAP_AMPDU_FACTOR {
  * According to IEEE802.11n spec size varies from 8K to 64K (in powers of 2)
  */
 #define IEEE80211_MIN_AMPDU_BUF 0x8
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,19,0))
 #define IEEE80211_MAX_AMPDU_BUF 0x40
+#endif
 
 
 /* Spatial Multiplexing Power Save Modes */
