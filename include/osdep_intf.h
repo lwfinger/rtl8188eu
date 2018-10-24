e0d59adcf69a32d7ed6fea90a70c2a74f46c5118
Larry Finger <Larry.Finger@lwfinger.net>
2018-10-24 16:53:39 -0500

    rtl8188eu: Flatted os_dep/linux/ into os_dep/
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/osdep_intf.h
+++ include/osdep_intf.h
@@ -104,10 +104,10 @@ u16 rtw_recv_select_queue(struct sk_buff *skb);
 int rtw_ndev_notifier_register(void);
 void rtw_ndev_notifier_unregister(void);
 
-#include "../os_dep/linux/rtw_proc.h"
+#include "../os_dep/rtw_proc.h"
 
 #ifdef CONFIG_IOCTL_CFG80211
-	#include "../os_dep/linux/ioctl_cfg80211.h"
+	#include "../os_dep/ioctl_cfg80211.h"
 #endif /* CONFIG_IOCTL_CFG80211 */
 
 u8 rtw_rtnl_lock_needed(struct dvobj_priv *dvobj);
