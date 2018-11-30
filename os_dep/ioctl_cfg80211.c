32e894d44f5c39ca3e93667d9152146253690873
Larry Finger <Larry.Finger@lwfinger.net>
2018-11-30 16:55:08 -0600

    rtl8188eu: Fix missing divide routine for 32-bit systems
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -350,9 +350,9 @@ static const struct ieee80211_txrx_stypes
 static u64 rtw_get_systime_us(void)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20, 0))
-	ktime_t ts;
+	u64 ts;
 	ts = ktime_get_boottime();
-	return ts / 1000;
+	return do_div(ts, 1000);
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
