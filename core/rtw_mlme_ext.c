52853162f7cc73a8ceeb14be99da5ea75474f1cc
Larry Finger <Larry.Finger@lwfinger.net>
2019-08-01 14:39:02 -0500

    rtl8188eu: Fix "fall through" warnings
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -1400,7 +1400,7 @@ void mgt_dispatcher(_adapter *padapter, union recv_frame *precv_frame)
 			ptable->func = &OnAuth;
 		else
 			ptable->func = &OnAuthClient;
-	/* pass through */
+		__attribute__ ((fallthrough));/* FALL THRU */
 	case WIFI_ASSOCREQ:
 	case WIFI_REASSOCREQ:
 		_mgt_dispatcher(padapter, ptable, precv_frame);
--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -1749,6 +1749,7 @@ static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 	#if defined(CONFIG_P2P) && ((LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 37)) || defined(COMPAT_KERNEL_RELEASE))
 	case NL80211_IFTYPE_P2P_CLIENT:
 		is_p2p = true;
+		__attribute__ ((fallthrough));/* FALL THRU */
 	#endif
 	case NL80211_IFTYPE_STATION:
 		networkType = Ndis802_11Infrastructure;
@@ -1773,6 +1774,7 @@ static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 	#if defined(CONFIG_P2P) && ((LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 37)) || defined(COMPAT_KERNEL_RELEASE))
 	case NL80211_IFTYPE_P2P_GO:
 		is_p2p = true;
+		__attribute__ ((fallthrough));/* FALL THRU */
 	#endif
 	case NL80211_IFTYPE_AP:
 		networkType = Ndis802_11APMode;
