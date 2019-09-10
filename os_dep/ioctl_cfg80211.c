5f2c143ca27ad38732ab4192c30be8148fa3e732
Yevhen Fastiuk <yevfast@gmail.com>
2019-09-10 17:07:28 +0300

    cfg80211: Fixed unallocated stats

--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -2948,6 +2948,7 @@ void rtw_cfg80211_indicate_sta_assoc(struct adapter *padapter, u8 *pmgmt_frame, 
 		sinfo.filled = 0;
 		sinfo.assoc_req_ies = pmgmt_frame + WLAN_HDR_A3_LEN + ie_offset;
 		sinfo.assoc_req_ies_len = frame_len - WLAN_HDR_A3_LEN - ie_offset;
+		cfg80211_sinfo_alloc_tid_stats(&sinfo, GFP_KERNEL);
 		cfg80211_new_sta(ndev, GetAddr2Ptr(pmgmt_frame), &sinfo, GFP_ATOMIC);
 	}
 }
