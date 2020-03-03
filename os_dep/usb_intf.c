2141f244c3e750f679b7a6c0848828bb7626a407
fiadosau <61759453+fiadosau@users.noreply.github.com>
2020-03-03 23:59:05 +0300

    Update usb_intf.c

--- os_dep/usb_intf.c
+++ os_dep/usb_intf.c
@@ -60,6 +60,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(0x056E, 0x4008)}, /* Elecom WDC-150SU2M */
 	{USB_DEVICE(0x2357, 0x010c)}, /* TP-Link TL-WN722N v2 */
 	{USB_DEVICE(0x2357, 0x0111)}, /* TP-Link TL-WN727N v5.21 */
+	{USB_DEVICE(0x2C4E, 0x0102)}, /* MERCUSYS MW150US v2 */
 	{}	/* Terminating entry */
 };
 
