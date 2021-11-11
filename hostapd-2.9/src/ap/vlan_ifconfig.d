0572d04e6736c6d0d72a43f7bf12824d3baff700
Larry Finger <Larry.Finger@lwfinger.net>
2021-11-11 16:41:09 -0600

    rtl8188eu: Minor change to control_ap
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- hostapd-2.9/src/ap/vlan_ifconfig.d
+++
@@ -1,7 +0,0 @@
-../src/ap/vlan_ifconfig.o: ../src/ap/vlan_ifconfig.c \
- /home/finger/hostapd-2.9/src/utils/includes.h \
- /home/finger/hostapd-2.9/src/utils/build_config.h \
- /home/finger/hostapd-2.9/src/utils/common.h \
- /home/finger/hostapd-2.9/src/utils/os.h \
- /home/finger/hostapd-2.9/src/utils/wpa_debug.h \
- /home/finger/hostapd-2.9/src/utils/wpabuf.h ../src/ap/vlan_util.h
--- hostapd-2.9/src/utils/wpabuf.d
+++
@@ -1,3 +0,0 @@
-../src/utils/wpabuf.o: ../src/utils/wpabuf.c ../src/utils/includes.h \
- ../src/utils/build_config.h ../src/utils/common.h ../src/utils/os.h \
- ../src/utils/wpa_debug.h ../src/utils/wpabuf.h ../src/utils/trace.h
