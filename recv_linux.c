5b67bc47eab11ffbffb136959b7149e15da72ccb
Larry Finger <Larry.Finger@lwfinger.net>
2022-01-26 13:11:39 -0600

    rtl8188eu: Fix bug related to removal of GRO_DROP
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- recv_linux.c
+++ recv_linux.c
@@ -315,6 +315,7 @@ static int napi_recv(_adapter *padapter, int budget)
 			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_DROP)
 				rx_ok = true;
 #else
+			rtw_napi_gro_receive(&padapter->napi, pskb);
 			rx_ok = true;
 #endif
 			goto next;
