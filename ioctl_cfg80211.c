ca95522030078c3a093ebb5327025f2d70cfd488
Giulio Benetti <giulio.benetti@benettiengineering.com>
2022-10-17 23:18:37 +0200

    Re-introduce code for Linux version < 6.0.0.
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>

--- ioctl_cfg80211.c
+++ ioctl_cfg80211.c
@@ -706,6 +706,10 @@ check_bss:
 
 		RTW_INFO(FUNC_ADPT_FMT" call cfg80211_roamed\n", FUNC_ADPT_ARG(padapter));
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0))
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 0, 0)
+		roam_info.channel = notify_channel;
+		roam_info.bssid = cur_network->network.MacAddress;
+#endif
 		roam_info.req_ie =
 			pmlmepriv->assoc_req+sizeof(struct rtw_ieee80211_hdr_3addr)+2;
 		roam_info.req_ie_len =
