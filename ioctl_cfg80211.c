57bd21c563f2d55e707fdd15f128726a2fb61560
Larry Finger <Larry.Finger@lwfinger.net>
2023-04-09 12:54:37 -0500

    rtl8188eu: Possible fix for iwd crash
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- ioctl_cfg80211.c
+++ ioctl_cfg80211.c
@@ -2023,6 +2023,8 @@ static int rtw_cfg80211_set_probe_req_wpsp2pie(_adapter *padapter, char *buf, in
 	RTW_INFO("%s, ielen=%d\n", __func__, len);
 #endif
 
+	if (!pmlmepriv)
+		return -EINVAL;
 	if (len > 0) {
 		wps_ie = rtw_get_wps_ie(buf, len, NULL, &wps_ielen);
 		if (wps_ie) {
