3ff6f7d84cb5346ed8d5dc21056769f2e03746d9
Larry Finger <Larry.Finger@lwfinger.net>
2023-07-29 12:32:25 -0500

    rtl8188eu: Fix shadow copy to dev_addr
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- osdep_service.c
+++ osdep_service.c
@@ -1367,7 +1367,7 @@ int rtw_change_ifname(_adapter *padapter, const char *ifname)
 
 	rtw_init_netdev_name(pnetdev, ifname);
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 17, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 0)
 	memcpy(pnetdev->dev_addr, adapter_mac_addr(padapter), ETH_ALEN);
 #else
 	dev_addr_set(pnetdev, adapter_mac_addr(padapter));
